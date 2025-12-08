11f2b3780739d37f1a4de3c45b86f053e506c1c3
Nicholas Pilkington <nick@dronedeploy.com>
2025-12-07 22:07:32 -0800

    da8 8

--- 8/main.py
+++ 8/main.py
@@ -1,41 +1,6 @@
 from functools import reduce
 from operator import mul
-
-class DSU:
-    def __init__(self, elements):
-        self.parent = {elem: elem for elem in elements}
-        self.size = {elem: 1 for elem in elements}
-    
-    def find(self, x):
-        if self.parent[x] != x:
-            self.parent[x] = self.find(self.parent[x])
-        return self.parent[x]
-    
-    def union(self, x, y):
-        root_x = self.find(x)
-        root_y = self.find(y)
-        
-        if root_x == root_y:
-            return False
-        
-        if self.size[root_x] < self.size[root_y]:
-            root_x, root_y = root_y, root_x
-        
-        self.parent[root_y] = root_x
-        self.size[root_x] += self.size[root_y]
-        return True
-    
-    def get_component_sizes(self):
-        components = {}
-        for elem in self.parent:
-            root = self.find(elem)
-            if root not in components:
-                components[root] = 0
-            components[root] += 1
-        return sorted(components.values(), reverse=True)
-    
-    def num_components(self):
-        return len(set(self.find(elem) for elem in self.parent))
+from scipy.cluster.hierarchy import DisjointSet
 
 def read_input(filename):
     with open(filename) as f:
@@ -47,40 +12,42 @@ def read_input(filename):
                 coordinates.append((x, y, z))
         return coordinates
 
-def calculate_distance(coord1, coord2):
-    x1, y1, z1 = coord1
-    x2, y2, z2 = coord2
-    return ((x1 - x2)**2 + (y1 - y2)**2 + (z1 - z2)**2)**0.5
-
 def create_sorted_pairs(coordinates):
-    """Create all pairs of coordinates and sort by distance."""
-    pairs = []
-    for i in range(len(coordinates)):
-        for j in range(i + 1, len(coordinates)):
-            dist = calculate_distance(coordinates[i], coordinates[j])
-            pairs.append((dist, coordinates[i], coordinates[j]))
-    
-    pairs.sort(key=lambda x: x[0])
-    return pairs
+    return sorted([
+        (((coordinates[i][0] - coordinates[j][0])**2 + (coordinates[i][1] - coordinates[j][1])**2 + (coordinates[i][2] - coordinates[j][2])**2)**0.5, coordinates[i], coordinates[j])
+        for i in range(len(coordinates))
+        for j in range(i + 1, len(coordinates))
+    ])
 
 def solve_part_1(filename, best_k=1000):
     coordinates = read_input(filename)
     pairs = create_sorted_pairs(coordinates)
-    dsu = DSU(coordinates)
+    dsu = DisjointSet(coordinates)
     for i in range(min(best_k, len(pairs))):
         dist, coord1, coord2 = pairs[i]
-        dsu.union(coord1, coord2)
-    sizes = dsu.get_component_sizes()[:3]
+        dsu.merge(coord1, coord2)
+    
+    # Get component sizes
+    components = {}
+    for coord in coordinates:
+        root = dsu[coord]
+        if root not in components:
+            components[root] = 0
+        components[root] += 1
+    sizes = sorted(components.values(), reverse=True)[:3]
     return reduce(mul, sizes, 1)
 
 def solve_part_2(filename):
     coordinates = read_input(filename)
     pairs = create_sorted_pairs(coordinates)
-    dsu = DSU(coordinates)
+    dsu = DisjointSet(coordinates)
     for i in range(len(pairs)):
         dist, coord1, coord2 = pairs[i]
-        dsu.union(coord1, coord2)
-        if dsu.num_components() == 1:
+        dsu.merge(coord1, coord2)
+        
+        # Check if all coordinates are in one component
+        num_components = len(set(dsu[coord] for coord in coordinates))
+        if num_components == 1:
             return coord1[0] * coord2[0]
             
 if __name__ == "__main__":
